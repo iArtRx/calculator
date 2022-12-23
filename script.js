31f446bf0bd9fdc71c637bfa3be932b0858504c0
Art <iartrx@gmail.com>
2022-12-23 15:23:48 +0700

    basic html and logic

---
+++ script.js
@@ -0,0 +1,13 @@
+//DOM Selectors
+const body = document.body;
+const main = document.main;
+const display = document.querySelector(".display")
+
+operate = (a, b, op) => {
+    return op === '+' ? a + b
+    : op === '-' ? a - b
+    : op === '*' ? a * b
+    : op === '/' ? a / b
+    : "invalid"
+}
+
