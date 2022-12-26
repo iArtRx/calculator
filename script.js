b656157eb71beaa316558ee787b4eb911f8729aa
Art <iArtRx@protonmail.com>
2022-12-26 12:01:43 +0000

    add function to operator, opernads and clear buttons and display

--- script.js
+++ script.js
@@ -1,7 +1,96 @@
 //DOM Selectors
 const body = document.body;
 const main = document.main;
-const display = document.querySelector(".display")
+const operands = document.querySelectorAll(".operand");
+const operators = document.querySelectorAll(".operator");
+const decimal = document.querySelector("#decimal");
+const allClear = document.querySelector("#all-clear");
+const clear = document.querySelector("#clear");
+const percent = document.querySelector("percent");
+
+let currentInput = "";
+let previousInput = "";
+let currentOperator = "";
+let operatorSelected = false;
+let floatInput = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    for (operand of operands) {
+    
+        operand.addEventListener("click", (e) => {
+            number = e.target.textContent;
+
+            // Limits the number allowed to be input
+            if (currentInput.length < 11) {
+                currentInput += number;
+            }
+
+            displayCurrent();
+            displayPrevious();
+            
+        });
+    }
+
+    for (operator of operators) {
+        
+        operator.addEventListener("click", (e) => {
+            currentOperator = e.target.textContent;
+            console.log(currentOperator)
+            
+            if (operatorSelected == false) {
+                operatorSelected = true;
+                console.log(operatorSelected)
+            }
+            else if (operatorSelected == true) {
+                previousInput = currentInput + " " + currentOperator;
+                displayPrevious();
+                displayCurrent();
+                currentInput = "";
+            }
+
+            
+        })
+    }
+
+    decimal.addEventListener("click", (e) => {
+        // Makes sure that only one decimal point can be input
+        if (floatInput == false) {
+            currentInput += ".";
+        }
+        floatInput = true;
+    })
+    
+    allClear.addEventListener("click", () => {
+        reset();
+        displayCurrent();
+        displayPrevious();
+    })
+
+    clear.addEventListener("click", () => {
+        currentInput = currentInput.slice(0, -1);
+        displayCurrent();
+    })
+
+})
+
+displayCurrent = () => {
+    document.querySelector(".current-input").innerHTML = currentInput;
+}
+
+displayPrevious = () => {
+    document.querySelector(".previous-input").innerHTML = previousInput;
+}
+
+reset = () => {
+    currentInput = "";
+    previousInput = "";
+    operatorSelected = "";
+    floatInput = false;
+}
+
+displayCurrent(currentInput);
+displayPrevious(previousInput);
 
 operate = (a, b, op) => {
     return op === '+' ? a + b
@@ -10,6 +99,4 @@ operate = (a, b, op) => {
     : op === '/' ? a / b
     : "invalid"
 }
-
-
 
